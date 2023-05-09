faf0aee8d13503d87d4834417e62475257ea3470
hmbashar <hmbashar@gmail.com>
2023-05-10 01:18:24 +0600

    makrup and function changed

--- widget.php
+++ widget.php
@@ -45,32 +45,31 @@ class wp_Project_Cost_Calculator_Widget extends \Elementor\Widget_Base
 
 ?>
 
-
         <div class="cb-pricing-calculator">
             <div class="cb-pricing-cal-heading">
                 <h2>COMPLEXITY</h2>
             </div>
             <div class="cb-pricing-level">
                 <label for="cbLow">
-                    <input type="radio" id="cbLow" onclick="cbGetRangeValue()" name="cbPricingLevel" checked>
+                    <input type="radio" onchange="cbGetRangeValue()" id="cbLow" name="cbPricingLevel" checked>
                     Low
                 </label>
                 <label for="cbmedium">
-                    <input type="radio" id="cbmedium" onclick="cbGetRangeValue()" name="cbPricingLevel">
+                    <input type="radio" onchange="cbGetRangeValue()"  id="cbmedium" name="cbPricingLevel">
                     Medium
                 </label>
                 <label for="cbhigh">
-                    <input type="radio" id="cbhigh" onclick="cbGetRangeValue()" name="cbPricingLevel">
+                    <input type="radio" onchange="cbGetRangeValue()"  id="cbhigh" name="cbPricingLevel">
                     High
                 </label>
             </div>
             <div class="cb-pricing-cal-number-of-pages">
                 <div class="cb-pricing-cal-pages-top">
-                    <h2>NUMBER OF PAGES </h2>
+                    <h2>NUMBER OF PAGES</h2>
                     <p id="cb-pricing-range-selected-page"></p>
                 </div>
                 <div class="cb-pricing-cal-range-slider">
-                    <input id="cbPricingRangeSlider" onchange="cbGetRangeValue()" type="range" min="1" max="10" step="1" value="3">
+                    <input id="cbPricingRangeSlider" type="range" min="1" max="10" step="1" value="3">
                     <div class="cb-pricing-cal-range-bottom">
                         <p id="cb-min-pages">1</p>
                         <p id="cb-max-pages">10</p>
@@ -92,46 +91,37 @@ class wp_Project_Cost_Calculator_Widget extends \Elementor\Widget_Base
                 return document.getElementById(id).checked;
             }
 
-
             function cbGetRangeValue() {
-                // get range value
+                // Get range value
                 let cbGetRangeSliderValue = document.getElementById("cbPricingRangeSlider").value;
 
-                // get pages number selected
+                // Get pages number selected
                 let cbNumberOfPagesSelected = document.getElementById('cb-pricing-range-selected-page');
-
                 let cbTotalPrice = document.getElementById('cbTotalPrice');
 
-
-                // set pages number to selected number from range value
+                // Set pages number to selected number from range value
                 cbNumberOfPagesSelected.innerText = cbGetRangeSliderValue;
 
 
 
+                // Price set based on package level
+                let priceMultiplier = 100; // Default price multiplier for low package
 
-                // price set for low package
-                if (cbGetSelectedPack('cbLow')) {
-                    let cbLowPackPrice = cbGetRangeSliderValue * 100;
-                    cbTotalPrice.innerText = cbLowPackPrice;
-
-                }
-
-                // price set for medium package
                 if (cbGetSelectedPack('cbmedium')) {
-                    let cbMediumPackPrice = cbGetRangeSliderValue * 200;
-                    cbTotalPrice.innerText = cbMediumPackPrice;
-
+                    priceMultiplier = 200; // Update price multiplier for medium package
+                } else if (cbGetSelectedPack('cbhigh')) {
+                    priceMultiplier = 300; // Update price multiplier for high package
                 }
 
-                // price set for High package
-                if (cbGetSelectedPack('cbhigh')) {
-                    let cbHighackPrice = cbGetRangeSliderValue * 300;
-                    cbTotalPrice.innerText = cbHighackPrice;
-
-                }
+                // Calculate and set the total price
+                let totalPrice = cbGetRangeSliderValue * priceMultiplier;
+                cbTotalPrice.innerText = totalPrice;
             }
 
-            cbGetRangeValue();
+            // Add event listener for input changes on the range slider
+            document.getElementById("cbPricingRangeSlider").addEventListener('input', cbGetRangeValue);
+
+           cbGetRangeValue();
         </script>
 
 <?php
